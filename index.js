333889750684c81cc11f8c5eb2928573c7309104
Michael B. Young <mikebyoung80@comcast.net>
2018-03-27 17:34:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 // write your code below!
-function() happyHolidays{
-  return 
-  
+function happyHolidays(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
 }
